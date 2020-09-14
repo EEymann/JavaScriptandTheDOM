099dfd128658b9627c3de781944f8f73c5b6dccd
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-09-14 16:28:10 -0700

    Delete the li text upon click instead of mouseover.

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ const addItemButton = document.querySelector('button.addItemButton');
 const addItemInput = document.querySelector('input.addItemInput');
 const removeItemButton = document.querySelector('button.removeItemButton');
 
-//Delete the li text gets upon mouseover
-listDiv.addEventListener('mouseover', (event) => {
+//Delete the li text gets upon click instead of mouseover
+listDiv.addEventListener('click', (event) => {
   if (event.target.tagName == 'LI') {
   let li = event.target; //Get a reference to the target element in the event. In this case the 'li'.
   let ul = li.parentNode; //Get a reference to the target li element's parent, which would be the ul.
